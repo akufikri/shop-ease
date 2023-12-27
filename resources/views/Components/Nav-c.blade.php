6f5df2ea08a063182b99e66f95f10357e74e1400
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2023-12-27 23:27:45 +0700

    [Add = UI Frontend => (Slicing UI #1)]

---
+++ resources/views/Components/Nav-c.blade.php
@@ -0,0 +1,83 @@
+<nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto">
+        <div class="flex justify-end">
+            <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                @foreach ($menuItems as $menuItem)
+                    <li>
+                        <a href="{{ $menuItem['url'] }}"
+                            class="text-gray-900 dark:text-white hover:underline {{ request()->is($menuItem['url']) ? 'font-bold' : '' }}">
+                            {{ $menuItem['name'] }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<nav class="bg-gray-50 border-gray-200 dark:bg-gray-900 px-8">
+    <div class="flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Shopease</span>
+        </a>
+
+        <div class="flex  w-full max-w-5xl">
+            <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
+                aria-expanded="false"
+                class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+                <span class="sr-only">Search</span>
+            </button>
+
+            <div class="relative hidden md:block w-full">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                    <span class="sr-only">Search icon</span>
+                </div>
+                <input type="text" id="search-navbar"
+                    class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 transition bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search...">
+            </div>
+
+            <button data-collapse-toggle="navbar-search" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-search" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+        </div>
+
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+            <div class="relative mt-3 md:hidden">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input type="text" id="search-navbar"
+                    class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search...">
+            </div>
+
+        </div>
+        <button type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium transition text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <i class="fa-solid fa-cart-shopping"></i>
+        </button>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\authController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\TypeProdukController;
@@ -19,40 +20,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [authController::class, 'login'])->name('login');
 Route::post('/login/proccess', [authController::class, 'processLogin']);
-
 Route::get('/register', [authController::class, 'register']);
 Route::post('/register/proccess', [authController::class, 'processRegistration']);
-
 Route::get('/logout', [authController::class, 'logout']);
 
 Route::get('/', function () {
-    return view('backend.page.dashboard.index');
+    return view('frontend.template');
 });
 
+Route::middleware(['auth', 'auth.admin'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 
-Route::prefix('kategori')->group(function () {
-    Route::get('/', [KategoriController::class, 'index']);
-    Route::get('/get', [KategoriController::class, 'getData']);
-    Route::get('/show/{id}', [KategoriController::class, 'showData']);
-    Route::delete('/delete/{id}', [KategoriController::class, 'deleteData']);
-    Route::put('/update/{id}', [KategoriController::class, 'update']);
-    Route::post('/create', [KategoriController::class, 'create']);
-});
+    Route::prefix('kategori')->group(function () {
+        Route::get('/', [KategoriController::class, 'index']);
+        Route::get('/get', [KategoriController::class, 'getData']);
+        Route::get('/show/{id}', [KategoriController::class, 'showData']);
+        Route::delete('/delete/{id}', [KategoriController::class, 'deleteData']);
+        Route::put('/update/{id}', [KategoriController::class, 'update']);
+        Route::post('/create', [KategoriController::class, 'create']);
+    });
 
-Route::prefix('produk')->group(function () {
-    Route::get('/', [ProdukController::class, 'index']);
-    Route::post('/store', [ProdukController::class, 'store']);
-    Route::get('/get', [ProdukController::class, 'get']);
-    Route::delete('/delete/{id}', [ProdukController::class, 'destroy']);
-    Route::put('/update/{id}', [ProdukController::class, 'update']);
-    Route::get('/show/{id}', [ProdukController::class, 'edit']);
-});
+    Route::prefix('produk')->group(function () {
+        Route::get('/', [ProdukController::class, 'index']);
+        Route::post('/store', [ProdukController::class, 'store']);
+        Route::get('/get', [ProdukController::class, 'get']);
+        Route::delete('/delete/{id}', [ProdukController::class, 'destroy']);
+        Route::put('/update/{id}', [ProdukController::class, 'update']);
+        Route::get('/show/{id}', [ProdukController::class, 'edit']);
+    });
 
-Route::prefix('type')->group(function () {
-    Route::get('/', [TypeProdukController::class, 'index']);
-    Route::get('/get', [TypeProdukController::class, 'getData']);
-    Route::get('/show/{id}', [TypeProdukController::class, 'showData']);
-    Route::delete('/delete/{id}', [TypeProdukController::class, 'deleteData']);
-    Route::put('/update/{id}', [TypeProdukController::class, 'update']);
-    Route::post('/create', [TypeProdukController::class, 'create']);
+    Route::prefix('type')->group(function () {
+        Route::get('/', [TypeProdukController::class, 'index']);
+        Route::get('/get', [TypeProdukController::class, 'getData']);
+        Route::get('/show/{id}', [TypeProdukController::class, 'showData']);
+        Route::delete('/delete/{id}', [TypeProdukController::class, 'deleteData']);
+        Route::put('/update/{id}', [TypeProdukController::class, 'update']);
+        Route::post('/create', [TypeProdukController::class, 'create']);
+    });
 });
